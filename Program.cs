dff6d3069d8b8bfbcac839826e5d5010b1d98c95
alfianbay26 <alfianmuh75@gmail.com>
2025-03-17 19:57:48 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_class_taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "jono";
+            taxi.OnDuty = "Yes";
+            taxi.NumPassenger = 10;
+
+            taxi.Registrasi();
+            taxi.Isipenumpang();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace tugas_class_taxi
+{
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public string OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        public void Registrasi()
+        {
+            Console.WriteLine("DriveName: {0}", DriverName);
+            Console.WriteLine("OnDuty: {0}", OnDuty);
+            Console.WriteLine("NumPassenger: {0}", NumPassenger);
+            Console.WriteLine("\nsedang menjemput penumpang");
+        }
+
+        public void Isipenumpang()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
